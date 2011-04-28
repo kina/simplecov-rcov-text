1c7f5402a1989d4676a1c999369e07898e726e09
William Kina <thekina@gmail.com>
2011-04-28 12:44:14 -0300

    changed README

--- lib/simplecov-rcov-text/version.rb
+++ lib/simplecov-rcov-text/version.rb
@@ -1,7 +1,7 @@
 module SimpleCov
   module Formatter
     class RcovTextFormatter
-      VERSION = "0.0.1"
+      VERSION = "0.0.2"
       UPSTREAM_URL = "https://github.com/kina/simplecov-rcov-text"
     end
   end
